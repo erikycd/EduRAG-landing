2837b1159770579904d66b0d3198ff3b6005eb29
Erik Degante <erikycd@gmail.com>
2026-01-26 19:45:23 -0600

    v0..0.1
    v0.0.1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Load Lottie animations
+lottie.loadAnimation({
+  container: document.getElementById('lottie-animation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://lottie.host/60b07be6-6d95-4060-af54-c05dfd51682e/tYWGY8TFHU.json'
+});
+
+lottie.loadAnimation({
+  container: document.getElementById('lottie-animation2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://lottie.host/b3a1e6d9-1fc5-4311-8cd7-468cffcc0704/QbMfAY1zVF.json'
+});
